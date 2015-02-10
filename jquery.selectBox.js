20f381186fec9e0c526cd9bee7161a1c12f2061d
marco <marco>
2015-02-10 10:58:27 +0100

    umdfy https://github.com/umdjs/umd/blob/master/jqueryPlugin.js

--- jquery.selectBox.js
+++ jquery.selectBox.js
@@ -7,7 +7,15 @@
  *
  * https://github.com/marcj/jquery-selectBox
  */
-;(function ($) {
+(function (factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['jquery'], factory);
+	} else {
+		// Browser globals
+		factory(jQuery);
+	}
+})(function ($) {
 
     /**
      * SelectBox class.
@@ -1138,4 +1146,4 @@
             return $(this);
         }
     });
-})(jQuery);
+});
